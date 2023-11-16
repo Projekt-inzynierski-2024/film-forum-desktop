dd0a84dcb3248d78cea9bc3d958a59409c4d225c
jangildev <jan.gil@student.up.krakow.pl>
2023-11-16 23:06:47 +0100

    pages init, general refactor needed

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,128 @@
 mod models;
 
-use iced::widget::{column, scrollable, text_input};
-use iced::{Application, Command, Length, Settings, Theme};
-use models::{ApiError, Film};
+use iced::widget::{button, column, scrollable, text_input};
+use iced::{Application, Command, Element, Length, Settings, Theme};
+
+use models::film::{Film, SearchError};
+use models::user::{LoginError, LoginResult, User};
 
 fn main() -> iced::Result {
     FilmForum::run(Settings::default())
 }
 
+struct Pages {
+    pages: Vec<Page>,
+    current: usize,
+}
+
+impl Pages {
+    fn new() -> Pages {
+        Pages {
+            current: 0,
+            pages: vec![
+                Page::Login {
+                    user: User::empty(),
+                },
+                Page::Search {
+                    query: String::new(),
+                    films: vec![],
+                },
+            ],
+        }
+    }
+
+    fn update(&mut self, msg: PageMessage) -> iced::Command<PageMessage> {
+        self.pages[self.current].update(msg)
+    }
+
+    fn view(&self) -> Element<PageMessage> {
+        self.pages[self.current].view()
+    }
+}
+
+enum Page {
+    Login { user: User },
+    Search { query: String, films: Vec<Film> },
+}
+
+impl Page {
+    fn update(&mut self, msg: PageMessage) -> iced::Command<PageMessage> {
+        match msg {
+            PageMessage::SearchFound(Ok(result)) => {
+                if let Page::Search { films, .. } = self {
+                    *films = result;
+                }
+                Command::none()
+            }
+            PageMessage::SearchFound(Err(_)) => Command::none(),
+            PageMessage::Search(text_query) => {
+                if let Page::Search { query, .. } = self {
+                    *query = text_query.clone();
+                }
+
+                Command::perform(Film::search(text_query), PageMessage::SearchFound)
+            }
+            PageMessage::Login => todo!(),
+            PageMessage::Email(email) => {
+                if let Page::Login { user } = self {
+                    user.email = email
+                }
+
+                Command::none()
+            }
+            PageMessage::Password(password) => {
+                if let Page::Login { user } = self {
+                    user.password = password;
+                }
+
+                Command::none()
+            }
+            PageMessage::LoggedIn(result) => {
+                println!("TOKEN: {}", result.unwrap().jwt_token);
+                Command::none()
+            }
+        }
+    }
+
+    fn view(&self) -> Element<PageMessage> {
+        match self {
+            Page::Login { user } => column![
+                text_input("Email", &user.email).on_input(PageMessage::Email),
+                text_input("Password", &user.password).on_input(PageMessage::Password),
+                button("Login").on_press(PageMessage::Login)
+            ]
+            .into(),
+            Page::Search { query, films } => {
+                let mut content =
+                    column![text_input("Search for film", query).on_input(PageMessage::Search)];
+
+                for film in films {
+                    content = content.push(iced::widget::text(&film.title.clone()));
+                }
+
+                content.into()
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum PageMessage {
+    SearchFound(Result<Vec<Film>, SearchError>),
+    Search(String),
+    Login,
+    LoggedIn(Result<LoginResult, LoginError>),
+    Email(String),
+    Password(String),
+}
+
 struct FilmForum {
-    query: String,
-    films: Vec<Film>,
+    pages: Pages,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    FilmsFound(Result<Vec<Film>, ApiError>),
-    Search(String),
+    PageMessage(PageMessage),
 }
 
 impl Application for FilmForum {
@@ -28,8 +134,7 @@ impl Application for FilmForum {
     fn new(flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
         (
             FilmForum {
-                query: String::new(),
-                films: vec![],
+                pages: Pages::new(),
             },
             Command::none(),
         )
@@ -39,29 +144,14 @@ impl Application for FilmForum {
         String::from("FilmForum")
     }
 
-    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+    fn update(&mut self, message: Self::Message) -> Command<Message> {
         match message {
-            Message::FilmsFound(Ok(films)) => {
-                self.films = films;
-                Command::none()
-            }
-            Message::FilmsFound(Err(_)) => Command::none(),
-            Message::Search(query) => {
-                self.query = query;
-                let newq = String::clone(&self.query);
-                Command::perform(Film::search(newq), Message::FilmsFound)
-            }
+            Message::PageMessage(msg) => self.pages.update(msg).map(Message::PageMessage),
         }
     }
 
-    fn view(&self) -> iced::Element<Message> {
-        let mut content =
-            column![text_input("Search for film", &self.query).on_input(Message::Search)];
-
-        for film in &self.films {
-            let ntit = String::clone(&film.title);
-            content = content.push(iced::widget::text(ntit));
-        }
+    fn view(&self) -> Element<Message> {
+        let content = self.pages.view().map(Message::PageMessage);
 
         scrollable(content)
             .width(Length::Fill)
--- src/models.rs
+++ src/models.rs
@@ -1,126 +1,162 @@
-use reqwest::header::{ACCEPT, CONTENT_TYPE};
-use serde::{Deserialize, Serialize};
+pub mod film {
+    use serde::Deserialize;
 
-#[derive(Debug, Clone)]
-pub enum ApiError {
-    EmailExists,
-    UsernameExists,
-}
+    #[derive(Debug, Clone)]
+    pub enum SearchError {}
 
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Film {
-    pub id: String,
-    pub title: String,
-    pub description: String,
-    pub is_movie: bool,
-}
+    #[derive(Debug, Clone, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Film {
+        pub id: String,
+        pub title: String,
+        pub description: String,
+        pub is_movie: bool,
+    }
 
-impl Film {
-    pub async fn search(query: String) -> Result<Vec<Film>, ApiError> {
-        // TODO: error handling
+    impl Film {
+        pub async fn search(query: String) -> Result<Vec<Film>, SearchError> {
+            // TODO: error handling
 
-        let url = format!("http://127.0.0.1:5105/api/film/search/{query}");
-        let films_result = match reqwest::get(url).await {
-            Ok(response) => match response.text().await {
-                Ok(content) => {
-                    let films: Vec<Film> = serde_json::from_str(content.as_str()).unwrap_or(vec![]);
-                    films
-                }
+            let url = format!("http://127.0.0.1:5105/api/film/search/{query}");
+            let films_result = match reqwest::get(url).await {
+                Ok(response) => match response.text().await {
+                    Ok(content) => {
+                        let films: Vec<Film> =
+                            serde_json::from_str(content.as_str()).unwrap_or(vec![]);
+                        films
+                    }
+                    Err(_) => vec![],
+                },
                 Err(_) => vec![],
-            },
-            Err(_) => vec![],
-        };
+            };
 
-        Ok(films_result)
-    }
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct UserResult {
-    pub id: i32,
-    pub username: String,
-    pub email: String,
-    pub jwt: String,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct User {
-    pub username: String,
-    pub email: String,
-    pub password: String,
-    pub confirm_password: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub secret_key: Option<String>,
-}
-
-impl User {
-    pub fn new_user(username: &str, email: &str, password: &str, confirm_password: &str) -> User {
-        User {
-            username: String::from(username),
-            email: String::from(email),
-            password: String::from(password),
-            confirm_password: String::from(confirm_password),
-            secret_key: None,
-        }
-    }
-
-    pub fn new_admin(
-        username: &str,
-        email: &str,
-        password: &str,
-        confirm_password: &str,
-        secret_key: &str,
-    ) -> User {
-        User {
-            secret_key: Some(String::from(secret_key)),
-            ..User::new_user(username, email, password, confirm_password)
-        }
-    }
-
-    pub async fn register(self) -> Result<UserResult, ApiError> {
-        let url = "http://127.0.0.1:5105/register";
-        let client = reqwest::Client::new();
-
-        let request = client
-            .post(url)
-            .header(ACCEPT, "*/*")
-            .header(CONTENT_TYPE, "application/json")
-            .body(serde_json::to_string(&self).unwrap())
-            .send()
-            .await;
-
-        let response = match request {
-            Ok(res) => res.text().await.unwrap_or(String::new()),
-            Err(_) => String::new(),
-        };
-
-        match response.as_str() {
-            "Email already exists" => Err(ApiError::EmailExists),
-            "Username already exists" => Err(ApiError::UsernameExists),
-            json => Ok(serde_json::from_str(json).unwrap()),
+            Ok(films_result)
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{ApiError, User};
+pub mod user {
+    use reqwest::header::{ACCEPT, CONTENT_TYPE};
+    use serde::{Deserialize, Serialize};
 
-    #[tokio::test]
-    async fn test_register() {
-        let user = User::new_user("username", "username@mail.pl", "zaq1@WSX", "zaq1@WSX");
+    #[derive(Debug, Clone)]
+    pub enum RegisterError {
+        EmailError(String),
+        UsernameError(String),
+        PasswordError(String),
+    }
 
-        let register = user.register().await;
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct RegisterResult {
+        pub id: i32,
+        pub username: String,
+        pub email: String,
+        pub jwt: String,
+    }
 
-        match register {
-            Ok(result) => println!("JWT: {}", result.jwt),
-            Err(ApiError::EmailExists) => println!("Email is taken"),
-            Err(ApiError::UsernameExists) => println!("Username is taken"),
+    #[derive(Debug, Clone)]
+    pub enum LoginError {}
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct LoginResult {
+        pub id: i32,
+        pub username: String,
+        pub jwt_token: String,
+    }
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct User {
+        pub username: String,
+        pub email: String,
+        pub password: String,
+        pub confirm_password: String,
+    }
+
+    impl User {
+        pub fn new(username: &str, email: &str, password: &str, confirm_password: &str) -> User {
+            User {
+                username: String::from(username),
+                email: String::from(email),
+                password: String::from(password),
+                confirm_password: String::from(confirm_password),
+            }
         }
 
-        // TODO: do actual test
+        pub fn empty() -> User {
+            User {
+                username: String::new(),
+                email: String::new(),
+                password: String::new(),
+                confirm_password: String::new(),
+            }
+        }
+
+        pub async fn login(&self) -> Result<LoginResult, LoginError> {
+            let url = "http://127.0.0.1:5105/login";
+            let client = reqwest::Client::new();
+
+            let request = client
+                .post(url)
+                .header(ACCEPT, "*/*")
+                .header(CONTENT_TYPE, "application/json")
+                .body(serde_json::to_string(&self).unwrap())
+                .send()
+                .await;
+
+            let response = match request {
+                Ok(res) => res.text().await.unwrap_or(String::new()),
+                Err(_) => String::new(),
+            };
+
+            Ok(serde_json::from_str(response.as_str()).unwrap())
+        }
+
+        pub async fn register(&self) -> Result<RegisterResult, RegisterError> {
+            let url = "http://127.0.0.1:5105/register";
+            let client = reqwest::Client::new();
+
+            let request = client
+                .post(url)
+                .header(ACCEPT, "*/*")
+                .header(CONTENT_TYPE, "application/json")
+                .body(serde_json::to_string(&self).unwrap())
+                .send()
+                .await;
+
+            let response = match request {
+                Ok(res) => res.text().await.unwrap_or(String::new()),
+                Err(_) => String::new(),
+            };
+
+            match response.as_str() {
+                "Email already exists" => Err(RegisterError::EmailError(response)),
+                "Username already exists" => Err(RegisterError::UsernameError(response)),
+                json => Ok(serde_json::from_str(json).unwrap()),
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{LoginResult, User};
+
+        #[tokio::test]
+        async fn test_register() {
+            let user = User::new_user("username", "username@mail.pl", "zaq1@WSX", "zaq1@WSX");
+
+            let _ = user.register().await;
+
+            let login = user.login().await;
+
+            match login {
+                Ok(res) => println!("TOKEN: {}", res.jwt_token),
+                Err(_) => {}
+            }
+
+            // TODO: do actual test
+        }
     }
 }
