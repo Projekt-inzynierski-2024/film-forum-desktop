47856bf985a8cbc3c40ca859e97b18d01ea21735
jangildev <jan.gil@student.up.krakow.pl>
2023-11-19 08:58:48 +0100

    page controller

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod client;
 
-use iced::widget::{button, column, scrollable, text_input};
-use iced::{Application, Command, Element, Length, Settings, Theme};
+use iced::widget::{button, column, row, scrollable, text_input};
+use iced::{theme, Application, Command, Element, Length, Settings, Theme};
 
 use client::models::film::Film;
 use client::models::user::LoginResult;
@@ -12,15 +12,37 @@ fn main() -> iced::Result {
     FilmForum::run(Settings::default())
 }
 
+#[derive(Debug, Clone)]
+enum PageState {
+    LoginPage,
+    RegisterPage,
+    SearchPage,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct PageController {
+    current: usize,
+}
+
+impl PageController {
+    fn change_page(&mut self, state: PageState) {
+        self.current = match state {
+            PageState::LoginPage => 0,
+            PageState::RegisterPage => 1,
+            PageState::SearchPage => 2,
+        }
+    }
+}
+
 struct Pages {
     pages: Vec<Page>,
-    current: usize,
+    controller: PageController,
 }
 
 impl Pages {
     fn new() -> Pages {
         Pages {
-            current: 1,
+            controller: PageController { current: 0 },
             pages: vec![
                 Page::Login {
                     email: String::new(),
@@ -41,11 +63,11 @@ impl Pages {
     }
 
     fn update(&mut self, msg: PageMessage) -> iced::Command<PageMessage> {
-        self.pages[self.current].update(msg, &mut self.current)
+        self.pages[self.controller.current].update(msg, &mut self.controller)
     }
 
     fn view(&self) -> Element<PageMessage> {
-        self.pages[self.current].view()
+        self.pages[self.controller.current].view()
     }
 }
 
@@ -67,7 +89,11 @@ enum Page {
 }
 
 impl Page {
-    fn update(&mut self, msg: PageMessage, current_page: &mut usize) -> iced::Command<PageMessage> {
+    fn update(
+        &mut self,
+        msg: PageMessage,
+        page_controller: &mut PageController,
+    ) -> iced::Command<PageMessage> {
         match msg {
             PageMessage::SearchFound(Ok(result)) => {
                 if let Page::Search { films, .. } = self {
@@ -106,9 +132,7 @@ impl Page {
             }
             PageMessage::LoggedIn(result) => {
                 match result {
-                    Ok(data) => {
-                        println!("JWT: {}", data.jwt);
-                    }
+                    Ok(data) => page_controller.change_page(PageState::SearchPage),
                     Err(LoginError::CredentialsError(field)) => {
                         println!("{}", field);
                     }
@@ -140,9 +164,7 @@ impl Page {
             ),
             PageMessage::Registered(result) => {
                 match result {
-                    Ok(data) => {
-                        *current_page = 0;
-                    }
+                    Ok(data) => page_controller.change_page(PageState::SearchPage),
                     Err(LoginError::CredentialsError(field)) => {
                         println!("{}", field);
                     }
@@ -153,41 +175,74 @@ impl Page {
 
                 Command::none()
             }
+            PageMessage::ChangePage(page) => {
+                page_controller.change_page(page);
+                Command::none()
+            }
         }
     }
 
     fn view(&self) -> Element<PageMessage> {
         match self {
-            Page::Login { email, password } => column![
-                text_input("Email", &email).on_input(PageMessage::Email),
-                text_input("Password", &password)
-                    .password()
-                    .on_input(PageMessage::Password),
-                button("Login").on_press(PageMessage::Login(email.clone(), password.clone()))
-            ]
-            .into(),
+            Page::Login { email, password } => {
+                let mut content = column![
+                    text_input("Email", &email).on_input(PageMessage::Email),
+                    text_input("Password", &password)
+                        .password()
+                        .on_input(PageMessage::Password),
+                ];
+
+                content = content
+                    .push(
+                        row![
+                            button("Register")
+                                .style(theme::Button::Secondary)
+                                .on_press(PageMessage::ChangePage(PageState::RegisterPage)),
+                            button("Login")
+                                .on_press(PageMessage::Login(email.clone(), password.clone()))
+                        ]
+                        .spacing(5),
+                    )
+                    .spacing(5);
+
+                content.into()
+            }
             Page::Register {
                 username,
                 email,
                 password,
                 confirm_password,
-            } => column![
-                text_input("Username", &username).on_input(PageMessage::Username),
-                text_input("Email", &email).on_input(PageMessage::Email),
-                text_input("Password", &password)
-                    .password()
-                    .on_input(PageMessage::Password),
-                text_input("Confirm password", &confirm_password)
-                    .password()
-                    .on_input(PageMessage::ConfirmPassword),
-                button("Register").on_press(PageMessage::Register(
-                    username.clone(),
-                    email.clone(),
-                    password.clone(),
-                    confirm_password.clone()
-                ))
-            ]
-            .into(),
+            } => {
+                let mut content = column![
+                    text_input("Username", &username).on_input(PageMessage::Username),
+                    text_input("Email", &email).on_input(PageMessage::Email),
+                    text_input("Password", &password)
+                        .password()
+                        .on_input(PageMessage::Password),
+                    text_input("Confirm password", &confirm_password)
+                        .password()
+                        .on_input(PageMessage::ConfirmPassword),
+                ];
+
+                content = content
+                    .push(
+                        row![
+                            button("Login")
+                                .style(theme::Button::Secondary)
+                                .on_press(PageMessage::ChangePage(PageState::LoginPage)),
+                            button("Register").on_press(PageMessage::Register(
+                                username.clone(),
+                                email.clone(),
+                                password.clone(),
+                                confirm_password.clone()
+                            ))
+                        ]
+                        .spacing(5),
+                    )
+                    .spacing(5);
+
+                content.into()
+            }
             Page::Search { query, films } => {
                 let mut content =
                     column![text_input("Search for film", query).on_input(PageMessage::Search)];
@@ -214,6 +269,7 @@ enum PageMessage {
     ConfirmPassword(String),
     Register(String, String, String, String),
     Registered(Result<LoginResult, LoginError>),
+    ChangePage(PageState),
 }
 
 struct FilmForum {
